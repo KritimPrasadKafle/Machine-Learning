8a2495c2c35248fc80e349f909e6f450849ff060
KritimPrasadKafle <kritim10k@gmail.com>
2025-04-15 22:12:02 +0545

    Django setup

---
+++ Django/1/todo_project/todo/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Task
+
+# Create your views here.
+
+def task_list(request):
+  tasks = Task.objects.all()
+  output = "<h1>To-Do List</h1><ul>"
+  for task in tasks:
+    output += f"<li>{task.title} (Created: {task.created_at})</li>"
+  output += "</ul>"
+  return HttpResponse(output)
