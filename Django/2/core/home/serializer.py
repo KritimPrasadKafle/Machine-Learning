f834d86bd866cf45858f5f986b1a15de672a2a6a
KritimPrasadKafle <kritim10k@gmail.com>
2025-04-20 23:18:16 +0545

    Added Serializer in Python

--- Django/2/core/home/serializer.py
+++ Django/2/core/home/serializer.py
@@ -6,4 +6,11 @@ class PeopleSerializer(serializers.ModelSerializer):
   class Meta:
     model = Person
     fields = '__all__'
-     
+
+  def validate_age(self, data):
+    pass 
+
+  def validate(self, data):
+    if data['age'] < 18:
+      raise serializers.ValidationError('Age must be 18 or older')
+    return data     
