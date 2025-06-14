fbd2b223d402f6fde64aecac5779b626fd4b28fe
Kritim Prasad Kafle <kritim10k@gmail.com>
2025-06-14 19:18:20 +0545

    Numbers in python

---
+++ python/pythonTutorials/2-Integer.py
@@ -0,0 +1,46 @@
+num = 1
+print(type(num))
+
+print(3//2)
+
+print(3**2)
+
+print(3%2)
+
+# for i in range(1,10):
+#     if i % 2 == 0:
+        # print(i)
+
+
+num1 = 2
+num1 += 1
+print(num1)
+
+print(round(3.75))
+print(round(3.75,1))
+
+num_1 = 3
+num_2 = 2
+
+print(num_1 == num_2)
+
+print(num_1 != num_2)
+
+print(num_1 >= num_2)
+
+# print(dir(num))
+
+print(num.__floor__)
+print(float(num))
+
+num1 = '100'
+num2 = '200'
+
+#casting
+num_1 = int(num1)
+num_2 = int(num2)
+
+print(num_1 + num_2)
+
+
+
